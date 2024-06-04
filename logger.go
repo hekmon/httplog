51ba6ece4c0597055aff9026c16b966b86222889
Hekmon <3418467+hekmon@users.noreply.github.com>
2024-06-04 10:44:22 +0200

    simplify contructor

--- logger.go
+++ logger.go
@@ -5,8 +5,9 @@ import (
 	"sync/atomic"
 )
 
-const (
+var (
 	// DefaultBodyMaxRead is the default maximum number of bytes a body must have to be logged.
+	// Its value is copied in the New() constructor.
 	DefaultBodyMaxRead int64 = 10000
 )
 
@@ -20,15 +21,12 @@ type Logger struct {
 }
 
 // New creates a new HTTP request/response logging utility.
-func New(logger *slog.Logger, debugBodyMaxRead int64) (l *Logger) {
+func New(logger *slog.Logger) (l *Logger) {
 	if logger == nil {
 		return
 	}
-	if debugBodyMaxRead < 0 {
-		debugBodyMaxRead = DefaultBodyMaxRead
-	}
 	return &Logger{
 		slogger:     logger,
-		bodyMaxRead: debugBodyMaxRead,
+		bodyMaxRead: DefaultBodyMaxRead,
 	}
 }
